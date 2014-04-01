a59c5abc67e5addac51f94b61b99735e1489822a
dsns <vantedur@icloud.com>
2014-04-01 16:32:18 -0700

    NSApplication Preferences Panel
    NSApplication  Blank-Preferences-Panel

---
+++ DSNS/DSNS/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DSNS
+//
+//  Created by Ksenofontov Misha on 01/04/14.
+//  Copyright (c) 2014 ___DSNS___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
